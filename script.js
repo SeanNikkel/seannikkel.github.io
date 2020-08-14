987a2b71cc2afc1b8c939fd4a6dfe601e94c0ac4
SeanNikkel <seann064@gmail.com>
2020-08-13 22:39:33 -0600

    Added parallax stars background

--- script.js
+++ script.js
@@ -20,3 +20,94 @@ function jumpTo(elementId)
     window.scrollTo(0, elementTop - navHeight - marginSize);
     return false;
 }
+
+// Stars background
+
+// create container element
+let starContainer = document.createElement("div");
+document.body.appendChild(starContainer);
+
+// functions
+function lerp(start, end, t) {
+    return (end - start) * t + start;
+}
+
+// classes
+function Star(x, y, depth) {
+    // Member variables
+    this.x = x;
+    this.y = y;
+    this.depth = depth;
+    this.moveFactor = lerp(0.005, 0.05, this.depth);
+
+    // Member functions
+    this.move = function(x, y) {
+        this.x += x * this.moveFactor;
+        this.y += y * this.moveFactor;
+
+        if (this.x < -3)
+            this.x += window.innerWidth + 6;
+
+        this.element.style.left = String(this.x) + "px";
+        this.element.style.top = String(this.y) + "px";
+    }
+    this.destroy = function() {
+        this.element.remove();
+    }
+
+    // Element init
+    this.element = document.createElement("div");
+    this.element.style.position = "fixed";
+    this.element.style.left = String(this.x) + "px";
+    this.element.style.top = String(this.y) + "px";
+    this.element.style.width = String(lerp(2, 6, this.depth)) + "px";
+    this.element.style.height = this.element.style.width;
+    this.element.style.backgroundColor = "rgb(48,48,48)";
+    this.element.style.zIndex = -100;
+    starContainer.appendChild(this.element);
+}
+
+// init
+let stars = [];
+function init() {
+    // clear current stars
+    for (let i = 0; i < stars.length; i++)
+    {
+        stars[i].destroy();
+    }
+
+    let starCount = Math.min((window.innerWidth * window.innerHeight) / 8000, 500);
+    for (let i = 0; i < starCount; i++)
+    {
+        stars.push(new Star(Math.random() * window.innerWidth, Math.random() * window.innerHeight, Math.random() ** 3));
+    }
+}
+init();
+
+// call init again after resize has stopped for 50ms
+let waitFunction;
+window.addEventListener("resize", function() { 
+    clearTimeout(waitFunction); 
+    waitFunction = setTimeout(init, 50); 
+}); 
+
+// update loop
+window.requestAnimationFrame(updateStars);
+let lastTime;
+function updateStars(time)
+{
+    // Get delta time
+    if (lastTime === undefined)
+        lastTime = time;
+    
+    let dt = time - lastTime;
+    lastTime = time;
+
+    // Move all stars left
+    for (let i = 0; i < stars.length; i++)
+    {
+        stars[i].move(-dt, 0);
+    }
+
+    window.requestAnimationFrame(updateStars);
+};
